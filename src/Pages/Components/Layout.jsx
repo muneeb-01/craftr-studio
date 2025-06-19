48e55e5540fd5183c76def7e874eecd958671061
muneeb-01 <muneerahmed8556@gmail.com>
2025-06-19 23:29:17 +0500

    commit

--- src/Pages/Components/Layout.jsx
+++ src/Pages/Components/Layout.jsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import FollowCursor from "./FollowCursor";
 const Layout = () => {
+  const [isMenu, setIsMenu] = useState(false);
   return (
     <>
-    <div className="relative overflow-x-hidden w-full h-full">
-     <FollowCursor/>
-      <Navbar/>
-      <Outlet />
-      <Footer />
-    </div>
+      <div className="relative overflow-x-hidden w-full h-full">
+        <FollowCursor />
+        <Navbar isMenu={isMenu} setIsMenu={setIsMenu} />
+        {!isMenu && (
+          <>
+            <Outlet /> <Footer />
+          </>
+        )}
+      </div>
     </>
   );
 };
